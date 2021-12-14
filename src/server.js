20eb368d96c42cfd846e57d8e770abd59cef22a1
Kingsenal <kwonddong2@gmail.com>
2021-12-14 21:32:41 +0900

    h

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 const handleHome = (req, res) => {
     return res.end();
 }
-
+//dd
 app.get("/", handleHome)
 
 app.listen(PORT,  () => console.log(`Server Listening on port http://localhost:${PORT}`)); // 서버가 시작될 때 작동하는 함수. port와 콜백함수가 필요함.
