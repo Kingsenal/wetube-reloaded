c58f39dc56a0b632a1c71dd7b988ccd93121b1fa
Kingsenal <kwonddong2@gmail.com>
2021-12-15 23:31:35 +0900

    a

--- src/server.js
+++ src/server.js
@@ -4,10 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next();
+};
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    next();
+}
+
 const handleHome = (req, res) => {
     return res.end();
 }
-//dd
-app.get("/", handleHome)
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome")
+}
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected)
 
 app.listen(PORT,  () => console.log(`Server Listening on port http://localhost:${PORT}`)); // 서버가 시작될 때 작동하는 함수. port와 콜백함수가 필요함.
