416897e80580e76ff1ccf17334bf257e9b9b5409
Kingsenal <kwonddong2@gmail.com>
2021-12-20 22:45:58 +0900

    h

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wetube Reloaded
+
+-글로벌 라우터
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+
+/users/edit -> Edit User
+/users/delete -> Delete User
+
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+
+//라우터를 도메인별로 나누는 것이 효율적
+//URL을 깔끔하게 보이기 위해서 규칙의 예외가 있을 수 있음  ex) /users/login 이 아니라 /login
--- src/server.js
+++ src/server.js
@@ -1,36 +1,20 @@
 import express from "express";
 import morgan  from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter  from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
-    next();
-};
-
-const privateMiddleware = (req, res, next) => { // controller
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    next();
-}
-//dd
-const handleHome = (req, res) => {
-    return res.end();
-}
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome")
-}
-
-app.use(morgan("dev"))
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", handleHome);
-app.get("/protected", handleProtected)
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-app.listen(PORT,  () => console.log(`Server Listening on port http://localhost:${PORT}`)); // 서버가 시작될 때 작동하는 함수. port와 콜백함수가 필요함.
+const handleListening = () => console.log(`Server Listening on port http://localhost:${PORT}`)
+
+app.listen(PORT, handleListening); // 서버가 시작될 때 작동하는 함수. port와 콜백함수가 필요함.
